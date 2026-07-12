d5d0c8ecedbd128bd9ec358d278f653691e0ccf1
foreverlove7914 <foreverlove7914@gmail.com>
2026-07-12 17:19:53 +0800

    Fix SYMBOL declaration format in config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const API_KEY = "136a7f84cee842b484cbb4a13327131a";
 const BASE_URL = "https://api.twelvedata.com";
 
 // Simbol yang digunakan
-const SYMBOL = XAUUSD:CUR";
+const SYMBOL = "XAUUSD";
 
 // Refresh setiap 5 saat
 const REFRESH_INTERVAL = 5000;
