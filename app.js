d32b0045cc1845225d6a10a2d1445c5935e55b91
foreverlove7914 <foreverlove7914@gmail.com>
2026-07-12 16:57:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
+async function loadPrice() {
 
+    try {
+
+        const url = `${BASE_URL}/price?symbol=${SYMBOL}&apikey=${API_KEY}`;
+
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        if(data.price){
+
+            document.getElementById("price").innerHTML =
+                "$" + Number(data.price).toFixed(2);
+
+            document.getElementById("trend").innerHTML =
+                "🟢 LIVE";
+
+            document.getElementById("signal").innerHTML =
+                "Menunggu Analisis...";
+
+        }else{
+
+            document.getElementById("price").innerHTML =
+                "API ERROR";
+
+            console.log(data);
+
+        }
+
+    }catch(error){
+
+        console.log(error);
+
+        document.getElementById("price").innerHTML =
+            "Connection Error";
+
+    }
+
+}
+
+loadPrice();
+
+setInterval(loadPrice, REFRESH_INTERVAL);
