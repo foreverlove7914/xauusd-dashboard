40b52fdffde1bdcd062392e8c6e1d00249428a28
foreverlove7914 <foreverlove7914@gmail.com>
2026-07-12 17:20:49 +0800

    Update SYMBOL format to include a slash

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const API_KEY = "136a7f84cee842b484cbb4a13327131a";
 const BASE_URL = "https://api.twelvedata.com";
 
 // Simbol yang digunakan
-const SYMBOL = "XAUUSD";
+const SYMBOL = "XAU/USD";
 
 // Refresh setiap 5 saat
 const REFRESH_INTERVAL = 5000;
