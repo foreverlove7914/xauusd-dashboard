dd53339e11fa3d3ac741070838f04a2f6f3c4b3c
foreverlove7914 <foreverlove7914@gmail.com>
2026-07-12 17:54:10 +0800

    Update indicators.js

--- indicators.js
+++ indicators.js
@@ -1,2 +1,9 @@
-// Indicator akan ditambah pada langkah seterusnya
-console.log("Indicators Loaded");
+document.getElementById("ema20").innerHTML = "🟢 BUY";
+document.getElementById("ema50").innerHTML = "🟢 BUY";
+document.getElementById("ema200").innerHTML = "🟢 BUY";
+
+document.getElementById("rsi").innerHTML = "62 (Neutral)";
+document.getElementById("macd").innerHTML = "🟢 BUY";
+
+document.getElementById("finalSignal").innerHTML =
+"🟢 STRONG BUY";
